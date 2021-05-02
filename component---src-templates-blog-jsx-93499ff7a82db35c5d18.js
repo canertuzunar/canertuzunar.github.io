84d27b61e07a9ec9ed635e84b79a9bc2ace8ee70
canertuzunar <176001027@stu.adu.edu.tr>
2021-05-02 22:38:25 +0300

    Updates

---
+++ component---src-templates-blog-jsx-93499ff7a82db35c5d18.js
@@ -0,0 +1,2 @@
+(self.webpackChunkpersonal_website=self.webpackChunkpersonal_website||[]).push([[366],{2975:function(e,t,n){"use strict";n.r(t);var l=n(7294),r=n(1089);t.default=function(e){var t=e.pageContext,n=t.frontmatter,a=t.html,u=t.markDown;return console.log(u),l.createElement(r.Ar,null,l.createElement("h1",null,n.title),l.createElement("h2",null,n.date),l.createElement("div",{dangerouslySetInnerHTML:{__html:a}}),l.createElement("div",null,u))}}}]);
+//# sourceMappingURL=component---src-templates-blog-jsx-93499ff7a82db35c5d18.js.map
