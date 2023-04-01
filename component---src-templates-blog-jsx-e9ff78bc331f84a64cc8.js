14635c2651d9167829549d0adfaca9b5311e8236
canertuzunar <tuzunarcaner@gmail.com>
2023-04-01 18:12:34 +0300

    hugo instead nextjs

--- component---src-templates-blog-jsx-e9ff78bc331f84a64cc8.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkpersonal_website=self.webpackChunkpersonal_website||[]).push([[366],{2975:function(e,t,n){"use strict";n.r(t);var l=n(7294),r=n(9973);t.default=function(e){var t=e.pageContext,n=t.frontmatter,a=t.html,u=t.markDown;return console.log(u),l.createElement(r.Ar,null,l.createElement("h1",null,n.title),l.createElement("h2",null,n.date),l.createElement("div",{dangerouslySetInnerHTML:{__html:a}}),l.createElement("div",null,u))}}}]);
-//# sourceMappingURL=component---src-templates-blog-jsx-e9ff78bc331f84a64cc8.js.map
